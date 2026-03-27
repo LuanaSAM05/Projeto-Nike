33e83db618b81a4c4799a716468f07d0369aa339
Luana Melo <lsilvaalvesmelo@gmail.com>
2026-03-27 17:41:45 +0000

    Atualizando

--- scripts.js
+++ scripts.js
@@ -1,20 +1,18 @@
 let body = document.querySelector("body");
 let tenis = document.querySelector(".imagem-tenis");
-let botaoCarrinho = document.querySelector(".botao-carrinho"); // botão do carrinho
 
 //document = html
 //querySelector = pegador / selecionador
 
 function mudarVisual(cor, imagem){
-   tenis.classList.add("troca-efeito")
+   tenis.classList.add("troca-efeito");
 
-   body.style.background = cor
-   botaoCarrinho.style.background = cor // muda a cor do botão
+   body.style.background = cor;
 
    // contador de tempo
    setTimeout(() => {
-        tenis.src = imagem
-        tenis.classList.remove("troca-efeito")
+        tenis.src = imagem;
+        tenis.classList.remove("troca-efeito");
    }, 500);
    // 1000 milissegundos = 1 segundo
 }
